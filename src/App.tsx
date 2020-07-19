f1f07b987482a041e5050f5e66c8743b326cd4d5
ingun <ingun37@gmail.com>
2020-07-19 17:37:59 +0900

    react-fullpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactFullpage from '@fullpage/react-fullpage';
 import { Skills } from "./Pages/Skills";
 import { Intro } from "./Pages/Intro";
 import { Chronicle } from "./Pages/Chronicle";
 import { Education } from "./Pages/Education";
-import 'fontsource-roboto';
 
 function App() {
+  const pages = [
+    Intro(), Skills(), Chronicle(), Education()
+  ]
   return (
-    <div className="App">
-      <Intro></Intro>
-      <Skills></Skills>
-      <Chronicle></Chronicle>
-      <Education></Education>
-    </div>
+    <ReactFullpage
+      //fullpage options
+      licenseKey={'YOUR_KEY_HERE'}
+      scrollingSpeed={1000} /* Options here */
+
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            {pages.map(x=>{
+              return (
+                <div className="section">
+                  {x}
+                </div>
+              )
+            })}
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   );
 }
 
+//Used recat-fullpage (https://github.com/alvarotrigo/react-fullpage)
 
 export default App;
