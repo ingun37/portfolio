7ada9d405f6c7cba4c24748efc801fcd6bfab180
Ingun <ingun37@gmail.com>
2020-07-26 20:32:27 +0900

    writings width

--- src/Pages/Writings.tsx
+++ src/Pages/Writings.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState, useRef, useLayoutEffect } from 'react'
 import Parser from 'rss-parser';
-import { List, ListItem, ListItemText, Chip, makeStyles } from '@material-ui/core';
+import { List, ListItem, ListItemText, Chip, makeStyles, Divider } from '@material-ui/core';
 import { on } from 'process';
 import { async } from 'rxjs';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    list: {
         maxHeight: '30vh',
         overflow: 'auto',
         msTouchAction: 'pan-y',
-        touchAction: 'pan-y'
+        touchAction: 'pan-y',
+        backgroundColor: theme.palette.background.paper,
+        maxWidth: '70vw'
     },
 }));
 class WritingModel {
@@ -25,13 +32,13 @@ function Writings() {
     const ref = React.createRef<HTMLDivElement>()
 
     useEffect(() => {
-        fetch("https://dev.to/feed/ingun37").then(x=>x.text()).then(x=>(new Parser()).parseString(x)).then(x=>{
+        fetch("https://dev.to/feed/ingun37").then(x => x.text()).then(x => (new Parser()).parseString(x)).then(x => {
             setModels(
                 x.items?.map(x => new WritingModel(x.title || "", x.categories || [])) || []
             )
         })
 
-        ref.current?.addEventListener('wheel', (e)=>{
+        ref.current?.addEventListener('wheel', (e) => {
             e.stopPropagation()
             return false
         })
@@ -41,15 +48,20 @@ function Writings() {
 
 
     return (
-        <div ref={ref}>
-            <List className={classes.root}>
+        <div ref={ref} className={classes.root}>
+            <List className={classes.list}>
                 {
                     models.map(model => {
                         return (
-                            <ListItem>
-                                <ListItemText primary={model.title} />
-                                {model.categories.map(x => (<Chip label={x} />))}
-                            </ListItem>)
+                            <div>
+
+                                <ListItem key={model.title}>
+                                    <ListItemText primary={model.title} />
+                                    {model.categories.map(x => (<Chip label={x} />))}
+                                </ListItem>
+                                <Divider />
+                            </div>
+                        )
                     })
                 }
             </List>
