a4d89c8076b9acc7482f44d9bb7879435b30fdac
Ingun <ingun37@gmail.com>
2020-08-04 00:24:04 +0900

    responsive

--- src/global/GlobalTheme.tsx
+++ src/global/GlobalTheme.tsx
@@ -1,37 +1,28 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { floor } from 'lodash';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
-const factor = 1.4;
-const remNum = (n:number)=> floor(n*factor, 2)
-const rem = (n:number)=>`${remNum(n)}rem`
-
-const globalTheme = createMuiTheme({
+const globalTheme = responsiveFontSizes(createMuiTheme({
     typography: {
         h2: {
-            fontSize: rem(1.5),
             fontFamily: [
                 'Anton',
             ].join(','),
         },
         h4: {
-            fontSize: rem(1),
             fontFamily: [
                 'Bebas Neue',
             ].join(','),
         },
         h5: {
-            fontSize: rem(0.83),
             fontFamily: [
                 'Fjalla One', 'Do Hyeon',
             ].join(','),
         },
         body1: {
-            fontSize: rem(0.83),
             fontFamily: [
                 'Fjalla One', 'Do Hyeon',
             ].join(','),
         }
     },
-});
+}));
 
 export default globalTheme;
