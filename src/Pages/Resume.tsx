57f6ee3ab2ed00e43f00068a0118399cc71cac7e
Ingun Jon <ingun37@gmail.com>
2021-08-11 18:35:28 +0900

    update resume

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import {ProjectCards, ProjectProp} from "../components/ProjectCards";
 import ResumePdf from '../resume.pdf';
-import ResumePortfolio from '../resume+portfolio.pdf';
 
 export default function Resume() {
     const games: ProjectProp[] = [
         new ProjectProp("resume", null, null, null, "Brief, on-point resume in both Korean & English", undefined, ResumePdf),
-        new ProjectProp("resume+portfolio", null, null, null, "Resume + descriptive portfolio in Korean", undefined, ResumePortfolio),
+        // new ProjectProp("resume+portfolio", null, null, null, "Resume + descriptive portfolio in Korean", undefined, ResumePortfolio),
     ];
     return (
         ProjectCards(games)
