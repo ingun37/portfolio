1715dbf04a74022357be70053ee875bfe0535c0a
ingunjon <ingun37@gmail.com>
2023-12-05 21:16:15 +0900

    add three.js contribution

--- src/Pages/Contributions.tsx
+++ src/Pages/Contributions.tsx
@@ -3,6 +3,7 @@ import {ProjectCards, ProjectProp} from "../components/ProjectCards";
 
 export function Contributions() {
     const games: ProjectProp[] = [
+        new ProjectProp("three.js", null, null, null, "Improved gaussian kernel usage of OutlinePass", undefined, "https://github.com/mrdoob/three.js/pull/24262"),
         new ProjectProp("goderive", null, null, null, "Code Generation for Functional Programming, Concurrency and Generics in Golang", undefined, "https://github.com/awalterschulze/goderive/pull/53"),
         new ProjectProp("iosMath", null, null, null, "Beautiful math equation rendering on iOS and MacOS", undefined, "https://github.com/kostub/iosMath/pull/121"),
     ];
