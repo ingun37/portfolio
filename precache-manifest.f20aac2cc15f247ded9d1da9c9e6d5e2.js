32ff09219ff25d79f8ac0416e97dfed23e311241
ingun37 <ingun37@users.noreply.github.com>
2023-12-06 00:47:14 +0000

    deploy: 0e9fcd12c7ed1297b150f341e9662de62392098e

---
+++ precache-manifest.f20aac2cc15f247ded9d1da9c9e6d5e2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9cf6f3c9598ec600095acea8f98069",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "809a01fa7fe246fdb06c",
+    "url": "/portfolio/static/css/main.966cf583.chunk.css"
+  },
+  {
+    "revision": "2aa5a710fb1301e04971",
+    "url": "/portfolio/static/js/2.32577924.chunk.js"
+  },
+  {
+    "revision": "af10f10b47d8516eee16b642ddcbd3d8",
+    "url": "/portfolio/static/js/2.32577924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809a01fa7fe246fdb06c",
+    "url": "/portfolio/static/js/main.6197daf7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "adac20d97acb728114c721bb50eab19c",
+    "url": "/portfolio/static/media/3dc.adac20d9.jpg"
+  },
+  {
+    "revision": "e7aadd2bde19e94b5ece80e873927dd0",
+    "url": "/portfolio/static/media/GitHub-Mark.e7aadd2b.svg"
+  },
+  {
+    "revision": "523b3ac7c185989612b0a64d524b61c5",
+    "url": "/portfolio/static/media/algebra.523b3ac7.png"
+  },
+  {
+    "revision": "38c4b670ee5c0b08545f16bfb5e9d889",
+    "url": "/portfolio/static/media/angular.38c4b670.svg"
+  },
+  {
+    "revision": "f33ee92e6eb3e238e0e3bb1c511034f2",
+    "url": "/portfolio/static/media/animation.f33ee92e.jpg"
+  },
+  {
+    "revision": "259e1339a21cf9a16881f722296cb704",
+    "url": "/portfolio/static/media/aws.259e1339.svg"
+  },
+  {
+    "revision": "79d65373176f95ae7d8cafc30015cab4",
+    "url": "/portfolio/static/media/closet.79d65373.jpg"
+  },
+  {
+    "revision": "7872b99cd79f20588f029567eb0f2266",
+    "url": "/portfolio/static/media/contest.7872b99c.jpeg"
+  },
+  {
+    "revision": "fad277796d6213cc33696da4666d80f5",
+    "url": "/portfolio/static/media/cru.fad27779.jpg"
+  },
+  {
+    "revision": "185ac1ad36fe10dd0ce90151a9142c85",
+    "url": "/portfolio/static/media/deferred.185ac1ad.jpg"
+  },
+  {
+    "revision": "130a162117a1f5f4c12410e2daa1ef91",
+    "url": "/portfolio/static/media/dgraph.130a1621.svg"
+  },
+  {
+    "revision": "9997aa6cfc5d55c35fafb1461d2724bf",
+    "url": "/portfolio/static/media/docker.9997aa6c.svg"
+  },
+  {
+    "revision": "eee87640929ef90fe1ce3010289d477a",
+    "url": "/portfolio/static/media/ebs.eee87640.png"
+  },
+  {
+    "revision": "c3e969050b614efa5110146aaade15fe",
+    "url": "/portfolio/static/media/eos.c3e96905.jpg"
+  },
+  {
+    "revision": "0cc6fe18b3cacb07954982a84585e5af",
+    "url": "/portfolio/static/media/firebase.0cc6fe18.svg"
+  },
+  {
+    "revision": "482e9be8c6076030a1415f79b66757d5",
+    "url": "/portfolio/static/media/firestore.482e9be8.svg"
+  },
+  {
+    "revision": "8d101afc44213e6f14b0f03f3d0498e5",
+    "url": "/portfolio/static/media/gcp.8d101afc.svg"
+  },
+  {
+    "revision": "44feb125362886d232dc47c540350b47",
+    "url": "/portfolio/static/media/grpc.44feb125.svg"
+  },
+  {
+    "revision": "40ef2fec6018b499a65e26b63db0227d",
+    "url": "/portfolio/static/media/kubernetes.40ef2fec.svg"
+  },
+  {
+    "revision": "20b874d5986d85aad47b300c72840723",
+    "url": "/portfolio/static/media/math.20b874d5.png"
+  },
+  {
+    "revision": "d59f4060add00ba5bc8e9323d521347f",
+    "url": "/portfolio/static/media/matrix.d59f4060.jpeg"
+  },
+  {
+    "revision": "a0fd85590b6d0031492e12f3d5cd0636",
+    "url": "/portfolio/static/media/react.a0fd8559.svg"
+  },
+  {
+    "revision": "4af097670e4c942373b440497993115a",
+    "url": "/portfolio/static/media/resume.4af09767.pdf"
+  },
+  {
+    "revision": "8a067f19c6a32f781b8cb971a3214629",
+    "url": "/portfolio/static/media/south-korea.8a067f19.svg"
+  },
+  {
+    "revision": "f3d3d99587bc6fa6de70e8df5b8fe2de",
+    "url": "/portfolio/static/media/toefl.f3d3d995.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/portfolio/static/media/usa.36ab476e.svg"
+  }
+]);
